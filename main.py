1de026a7224eeb6f2eb73208371fd759e2d42598
Muthumanikandan Alagirisamy <amuthumanikandan@athenahealth.com>
2024-06-24 15:28:12 +0530

    pushing the file

---
+++ main.py
@@ -0,0 +1,671 @@
+#list
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+#Python - Change List Items
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+#Change a Range of Item Values
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+#Change the second and third value by replacing it with one value:
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+
+#Python - Add List Items
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+#Insert Items
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+#Extend List
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+#The extend() method does not have to append lists, you can add any iterable object (tuples, sets, dictionaries etc.).
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
+
+#Python - Remove List Items
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+#Remove the first occurrence of "banana":
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist.remove("banana")
+print(thislist)
+
+#Remove Specified Index
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+#Remove the last item:
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+#Remove the first item:
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+#Delete the entire list:
+thislist = ["apple", "banana", "cherry"]
+del thislist
+#Clear the List
+#The clear() method empties the list.
+
+#Python - Loop Lists
+#You can loop through the list items by using a for loop:
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+
+#Loop Through the Index Numbers
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+
+#Using a While Loop
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+#Looping Using List Comprehension
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+#O/p - ['apple', 'banana', 'mango']
+
+#The Syntax
+newlist = [x for x in fruits if x != "apple"]
+#The condition if x != "apple"  will return True for all elements other than "apple", making the new list contain all fruits except "apple".
+
+#The iterable can be any iterable object, like a list, tuple, set etc.
+#newlist = [x for x in range(10)]
+#newlist1 = [x for x in range(10) if x < 5]
+#newlist2 = [x.upper() for x in fruits]
+
+#Return "orange" instead of "banana":
+newlist1 = [x if x != "banana" else "orange" for x in fruits]
+
+#Python - Sort Lists
+#List objects have a sort() method that will sort the list alphanumerically, ascending, by default:
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+#Sort the list numerically:
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
+# o/p - [23, 50, 65, 82, 100]
+
+#Sort Descending
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+#o/p- ['pineapple', 'orange', 'mango', 'kiwi', 'banana']
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist)
+#o/p = [100, 82, 65, 50, 23]
+
+#Customize Sort Function
+def myfunc(n):
+  return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+#o/p = [50, 65, 23, 82, 100]
+
+#Case Insensitive Sort
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort()
+print(thislist)
+#o/p- ['Kiwi', 'Orange', 'banana', 'cherry']
+
+#Perform a case-insensitive sort of the list:
+#thislist = ["banana", "Orange", "Kiwi", "cherry"]
+#thislist.sort(key = str.lower)
+#print(thislist)
+
+#Reverse the order of the list items:
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.reverse()
+print(thislist)
+#o/p - ['cherry', 'Kiwi', 'Orange', 'banana']
+
+#Python - Copy Lists
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+#o/p - ['apple', 'banana', 'cherry']
+
+#Make a copy of a list with the list() method:
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+#o/p - ['apple', 'banana', 'cherry']
+
+#Python - Join Lists
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+#o/p - ['a', 'b', 'c', 1, 2, 3]
+
+#Append list2 into list1:
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
+#o/p - ['a', 'b', 'c', 1, 2, 3]
+
+#Use the extend() method to add list2 at the end of list1:
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
+#o/p-['a', 'b', 'c', 1, 2, 3]
+
+#Python Tuples
+#Tuples are used to store multiple items in a single variable.
+#Create a Tuple:
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+#Tuple items are ordered, unchangeable, and allow duplicate values.
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+#Tuple Length
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+#One item tuple, remember the comma:
+thistuple = ("apple",)
+print(type(thistuple))
+#o/p - This is tuple
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+#Tuple Items - Data Types
+#String, int and boolean data types:
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+#A tuple can contain different data types:
+#tuple1 = ("abc", 34, True, 40, "male")
+#type(tuple1)
+#o/p - <class 'tuple'>
+
+#The tuple() Constructor
+#Using the tuple() method to make a tuple:
+thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(thistuple)
+
+#Access Tuple Items
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+#Note: The first item has index 0.
+
+#Negative Indexing
+#-1 refers to the last item, -2 refers to the second last item etc.
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+
+#Range of Indexes
+#Return the third, fourth, and fifth item:
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+#o/p - ('cherry', 'orange', 'kiwi')
+
+#This example returns the items from the beginning to, but NOT included, "kiwi":
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+
+#This example returns the items from "cherry" and to the end:
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+
+#Specify negative indexes if you want to start the search from the end of the tuple:
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+#o/p- ('orange', 'kiwi', 'melon')
+
+#Check if Item Exists
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+#o/p - Yes, 'apple' is in the fruits tuple
+
+#Python - Update Tuples
+#Tuples are unchangeable, meaning that you cannot change, add, or remove items once the tuple is created.
+#Convert the tuple into a list to be able to change it:
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+print(x)
+#o/p - ("apple", "kiwi", "cherry")
+
+#thistuple = ("apple", "banana", "cherry")
+#y = list(thistuple)
+#y.append("orange")
+#thistuple = tuple(y)
+
+#Add tuple to a tuple. You are allowed to add tuples to tuples, so if you want to add one item, (or many), create a new tuple with the item(s), and add it to the existing tuple:
+#thistuple = ("apple", "banana", "cherry")
+#y = ("orange",)
+#thistuple += y
+#print(thistuple)
+#o/p - ('apple', 'banana', 'cherry', 'orange')
+
+#Note: When creating a tuple with only one item, remember to include a comma after the item, otherwise it will not be identified as a tuple.
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+
+#The del keyword can delete the tuple completely:
+#thistuple = ("apple", "banana", "cherry")
+#del thistuple
+#print(thistuple) #this will raise an error because the tuple no longer exists
+
+#Python - Unpack Tuples
+#Packing a tuple:
+fruits = ("apple", "banana", "cherry")
+#But, in Python, we are also allowed to extract the values back into variables. This is called "unpacking":
+
+#Unpacking a tuple:
+#fruits = ("apple", "banana", "cherry")
+#(green, yellow, red) = fruits
+#print(green)
+#print(yellow)
+#print(red)
+#o/p - apple
+#banana
+#cherry
+
+#Using Asterisk*
+#If the number of variables is less than the number of values, you can add an * to the variable name and the values will be assigned to the variable as a list:
+#fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+#(green, yellow, *red) = fruits
+#print(green)
+#print(yellow)
+#print(red)
+#o/p - apple
+#banana
+#['cherry', 'strawberry', 'raspberry']
+
+#If the asterisk is added to another variable name than the last, Python will assign values to the variable until the number of values left matches the number of variables left.
+#fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+#(green, *tropic, red) = fruits
+#print(green)
+#print(tropic)
+#print(red)
+#o/p - apple
+#['mango', 'papaya', 'pineapple']
+#cherry
+
+#Python - Loop Tuples, Loop Through a Tuple
+#Iterate through the items and print the values:
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x)
+
+#Use the range() and len() functions to create a suitable iterable.
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print(thistuple[i])
+
+#To join two or more tuples you can use the + operator:
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+#o/p - ('a', 'b', 'c', 1, 2, 3)
+
+#Multiply Tuples
+#If you want to multiply the content of a tuple a given number of times, you can use the * operator:
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+print(mytuple)
+#o/p - ('apple', 'banana', 'cherry', 'apple', 'banana', 'cherry')
+
+#Python - Tuple Methods
+#Python has two built-in methods that you can use on tuples.
+#count()	Returns the number of times a specified value occurs in a tuple
+#index()	Searches the tuple for a specified value and returns the position of where it was found
+
+#Python Sets
+#Create a Set:
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+#Note: Sets are unordered, so you cannot be sure in which order the items will appear.
+#Duplicate values will be ignored:
+thisset = {"apple", "banana", "cherry", "apple"}
+print(thisset)
+
+#True and 1 is considered the same value:
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+print(thisset)
+
+#Note: The values False and 0 are considered the same value in sets, and are treated as duplicates:
+#False and 0 is considered the same value:
+thisset = {"apple", "banana", "cherry", False, True, 0}
+print(thisset)
+
+#Get the Length of a Set
+thisset = {"apple", "banana", "cherry"}
+print(len(thisset))
+
+#Set Items - Data Types
+#Set items can be of any data type:
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+
+#A set with strings, integers and boolean values:
+set1 = {"abc", 34, True, 40, "male"}
+
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+
+#The set() Constructor
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
+
+#Access Items
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+  print(x)
+
+# Check if "banana" is present in the set:
+thisset = {"apple", "banana", "cherry"}
+print("banana" in thisset)
+#Check if "banana" is NOT present in the set:
+thisset = {"apple", "banana", "cherry"}
+print("banana" not in thisset)
+
+#Change Items
+#Once a set is created, you cannot change its items, but you can add new items.
+#Python - Add Set Items
+#Add an item to a set, using the add() method:
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+#o/p - {'apple', 'cherry', 'banana', 'orange'}
+
+#Add Sets
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+thisset.update(tropical)
+print(thisset)
+
+#Add Any Iterable
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+thisset.update(mylist)
+
+#Python - Remove Set Items
+#Remove "banana" by using the remove() method:
+thisset = {"apple", "banana", "cherry"}
+thisset.remove("banana")
+print(thisset)
+
+#Remove "banana" by using the discard() method:
+thisset = {"apple", "banana", "cherry"}
+thisset.discard("banana")
+print(thisset)
+#Note: If the item to remove does not exist, discard() will NOT raise an error.
+
+#You can also use the pop() method to remove an item, but this method will remove a random item, so you cannot be sure what item that gets removed.
+#Remove a random item by using the pop() method:
+thisset = {"apple", "banana", "cherry"}
+x = thisset.pop()
+print(x)
+print(thisset)
+
+#The clear() method empties the set:
+thisset = {"apple", "banana", "cherry"}
+thisset.clear()
+print(thisset)
+
+#The del keyword will delete the set completely:
+#thisset = {"apple", "banana", "cherry"}
+#del thisset
+#print(thisset)
+
+#Python - Loop Sets
+#You can loop through the set items by using a for loop:
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+  print(x)
+
+#Python - Join Sets
+#The union() and update() methods joins all items from both sets.
+#The intersection() method keeps ONLY the duplicates.
+#The difference() method keeps the items from the first set that are not in the other set(s).
+#The symmetric_difference() method keeps all items EXCEPT the duplicates.
+
+#Join set1 and set2 into a new set:
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = set1.union(set2)
+print(set3)
+
+#Use | to join two sets:
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = set1 | set2
+print(set3)
+
+#Join multiple sets with the union() method:
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = {"John", "Elena"}
+set4 = {"apple", "bananas", "cherry"}
+myset = set1.union(set2, set3, set4)
+print(myset)
+
+#Use | to join two sets:
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = {"John", "Elena"}
+set4 = {"apple", "bananas", "cherry"}
+myset = set1 | set2 | set3 |set4
+print(myset)
+
+#Join a Set and a Tuple
+#The union() method allows you to join a set with other data types, like lists or tuples.
+x = {"a", "b", "c"}
+y = (1, 2, 3)
+z = x.union(y)
+print(z)
+
+#Update
+#The update() method inserts the items in set2 into set1:
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+set1.update(set2)
+print(set1)
+#Note: Both union() and update() will exclude any duplicate items.
+
+#Intersection
+#Keep ONLY the duplicates
+#Join set1 and set2, but keep only the duplicates:
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set3 = set1.intersection(set2)
+print(set3)
+#o/p - apple
+
+#Use & to join two sets:
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set3 = set1 & set2
+print(set3)
+
+# The & operator only allows you to join sets with sets, and not with other data types like you can with the intersection() method.
+#The intersection_update() method will also keep ONLY the duplicates, but it will change the original set instead of returning a new set.
+
+#Keep the items that exist in both set1, and set2:
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set1.intersection_update(set2)
+print(set1)
+
+#Join sets that contains the values True, False, 1, and 0, and see what is considered as duplicates:
+set1 = {"apple", 1,  "banana", 0, "cherry"}
+set2 = {False, "google", 1, "apple", 2, True}
+set3 = set1.intersection(set2)
+print(set3)
+
+#The difference() method will return a new set that will contain only the items from the first set that are not present in the other set.
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set3 = set1.difference(set2)
+print(set3)
+#o/p- {'banana', 'cherry'}
+
+#You can use the - operator instead of the difference() method, and you will get the same result.
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set3 = set1 - set2
+print(set3)
+
+#Note: The - operator only allows you to join sets with sets, and not with other data types like you can with the difference() method.
+#Use the difference_update() method to keep the items that are not present in both sets:
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set1.difference_update(set2)
+print(set1)
+
+#The symmetric_difference() method will keep only the elements that are NOT present in both sets.
+#Keep the items that are not present in both sets:
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set3 = set1.symmetric_difference(set2)
+print(set3)
+#o/p - {'google', 'banana', 'microsoft', 'cherry'}
+
+#Use ^ to join two sets:
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set3 = set1 ^ set2
+print(set3)
+
+#Note: The ^ operator only allows you to join sets with sets, and not with other data types like you can with the symmetric_difference() method.
+#Use the symmetric_difference_update() method to keep the items that are not present in both sets:
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set1.symmetric_difference_update(set2)
+print(set1)
+
+#Python Dictionaries
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+#Dictionaries are used to store data values in key:value pairs.
+#Create and print a dictionary:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+#Dictionary items are ordered, changeable, and do not allow duplicates.
+Print the "brand" value of the dictionary:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+
+#When we say that dictionaries are ordered, it means that the items have a defined order, and that order will not change.
+#Unordered means that the items do not have a defined order, you cannot refer to an item by using an index.
+#Dictionaries are changeable, meaning that we can change, add or remove items after the dictionary has been created.
+#Dictionaries cannot have two items with the same key:
+#Duplicate values will overwrite existing values:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+#o/p - {'brand': 'Ford', 'model': 'Mustang', 'year': 2020}
+
+#Dictionary Length
+#To determine how many items a dictionary has, use the len() function:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(len(thisdict))
+#o/p - 3
+
+#Dictionary Items - Data Types
+#The values in dictionary items can be of any data type:
+#String, int, boolean, and list data types:
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+#<class 'dict'>
+
+#The dict() Constructor
+
+
+
+
+
+
